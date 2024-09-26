5644fe2c1cc06bd8225ad77bc525048ba7506ab9
GeovanaCamilo <68429383+GeovanaCamilo@users.noreply.github.com>
2024-09-25 21:31:14 -0300

    Add files via upload

---
+++ Aulaprogorientada.py
@@ -0,0 +1,8 @@
+print ('O que e Python?')
+print('Python e uma linguagem de programação que visa o desenvolvimento de sistemas, sua caracteristica notavel e a "praticidade".')
+
+print ('Qual programa (IDE) executa o arquivo .py?')
+print ('VisualCode')
+
+print('Qual linguagem da programação Python substituiu?')
+print('Substituiu a linguagem Java')
